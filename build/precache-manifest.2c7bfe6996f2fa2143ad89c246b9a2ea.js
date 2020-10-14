5bda5f6cbe2566cfb2306a12a5f21f0e6a491083
Trevor Rocha <trevorrocha@gmail.com>
2020-10-14 19:42:50 -0400

    Commiting

---
+++ build/precache-manifest.2c7bfe6996f2fa2143ad89c246b9a2ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd51d96380a5031195fd",
+    "url": "/static/css/main.69920d76.chunk.css"
+  },
+  {
+    "revision": "dd51d96380a5031195fd",
+    "url": "/static/js/main.dd51d963.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4367c412e7c6c1a1f7153626f04731de",
+    "url": "/index.html"
+  }
+];
